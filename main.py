e7a7e89264deffa80a87ce132bf4373d69fc7486
rltran-codex <rtran.codex@gmail.com>
2025-12-03 17:29:23 -0500

    use function 1 and 2

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def function_2(x: int, y: int) -> int:
     return x - y
 
 
+def print_file(path: str) -> str:
+    try:
+        with open(path, "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "The file 'my_text_file.txt' was not found."
+
+
 if __name__ == "__main__":
     x = 10
     y = 25
     t = function_1(x, y)
+    print(t)
+    t = function_2(x, y)
+    print(t)
