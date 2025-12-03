4f56c58f5de9ac0fe42576e0a227abdc337c3902
rltran-codex <rtran.codex@gmail.com>
2025-12-03 17:24:55 -0500

    implement function 1 and 2

---
+++ main.py
@@ -0,0 +1,12 @@
+def function_1(x: int, y: int) -> int:
+    return x + y
+
+
+def function_2(x: int, y: int) -> int:
+    return x - y
+
+
+if __name__ == "__main__":
+    x = 10
+    y = 25
+    t = function_1(x, y)
